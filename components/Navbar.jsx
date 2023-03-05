d20739b754ff8493bca42f696a824b34615f5c73
Henoker <hennybany@gmail.com>
2023-03-05 20:20:55 +0300

    feat: navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,9 +1,37 @@
 'use client';
+import { motion } from 'framer-motion';
+import styles from '../styles';
+import {navVariants} from '../utils/motion';
+
+
 
 const Navbar = () => (
-  <nav>
-    navbar
-  </nav>
+  <motion.nav
+  variants={navVariants}
+  initial="hidden"
+  whileInView="show"
+  className={`${styles.xPaddings} py-8 relative`}
+  >
+    <div className='absolute w-[50%] inset-0 gradient-01'/>
+
+    <div className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}>
+      <img
+        src='/search.svg'
+        alt='search'
+        className='w-[24px] h-[24px] object-contain fill-inherit'
+      />
+      <img
+      src='/logo-horizontal-preta.png'
+      alt='logo'
+      className='w-[180px] h-[80px] object-contain'
+      />
+      <img 
+      src='/menu.svg'
+      alt='menu'
+      className='w-[24px] h-[24px] object-contain'
+      />
+    </div>
+  </motion.nav>
 );
 
 export default Navbar;

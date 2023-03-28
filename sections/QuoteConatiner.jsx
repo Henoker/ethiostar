9955bc798d3f1a38b3a03e49c2db92646d49caec
Henoker <hennybany@gmail.com>
2023-03-28 11:08:14 +0300

    feat: why choose us section

--- sections/QuoteConatiner.jsx
+++ sections/QuoteConatiner.jsx
@@ -235,6 +235,218 @@ const QuoteContainer = () => {
      </div> */}
     </div>
   </div>
+  <div className="mx-auto max-w-screen-xl px-4 py-8 sm:py-12 sm:px-6 lg:py-16 lg:px-8">
+      <div className="mx-auto max-w-lg text-center">
+        <h2 className="text-3xl font-bold sm:text-4xl text-white">
+          Why choose us
+        </h2>
+        <p className="mt-4 text-gray-300 text-justify">
+          We proudly provide over 200 languages by specializing in african languages. 
+          We serve Governments, international NGO's, young innovative startups and large global enterprises from varying industries.
+          We have optimized our processes to meet their unique needs, adapting from small, on-demand tasks to high-touch, fully managed solutions.
+        </p>
+      </div>
+      <div className="mt-8 grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
+        <a
+          className="block rounded-xl border border-gray-800 p-8 shadow-xl transition hover:border-teal-500/10 hover:shadow-teal-500/10"
+          href="/services/digital-campaigns"
+        >
+           <svg 
+          className="h-10 w-10 text-[#B3FF17]"
+          fill="none" 
+          stroke="currentColor" 
+          stroke-width="1.5" 
+          viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" 
+          aria-hidden="true">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 01-1.043 3.296 3.745 3.745 0 01-3.296 1.043A3.745 3.745 0 0112 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 01-3.296-1.043 3.745 3.745 0 01-1.043-3.296A3.745 3.745 0 013 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 011.043-3.296 3.746 3.746 0 013.296-1.043A3.746 3.746 0 0112 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 013.296 1.043 3.746 3.746 0 011.043 3.296A3.745 3.745 0 0121 12z"></path>
+          </svg>
+          <h2 className="mt-4 text-xl font-bold text-white">
+            Guaranteed Quality
+          </h2>
+          <p className="mt-1 text-sm text-gray-300">
+            We support the world&apos;s best translators with advanced quality assurance processes. And that&apos;s not all: we provide a free comprehensive translation review if you happen to be unsatisfied.
+          </p>
+        </a>
+        <a
+          className="block rounded-xl border border-gray-800 p-8 shadow-xl transition hover:border-teal-500/10 hover:shadow-teal-500/10"
+          href="/services/digital-campaigns"
+        >
+         <svg
+         className="h-10 w-10 text-[#B3FF17]"
+         fill="none" 
+         stroke="currentColor" 
+         stroke-width="1.5" 
+         viewBox="0 0 24 24" 
+         xmlns="http://www.w3.org/2000/svg" 
+         aria-hidden="true">
+         <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 18.75a60.07 60.07 0 0115.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 013 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414.336.75.75.75h.75m-1.5-1.5h.375c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-.375m1.5-1.5H21a.75.75 0 00-.75.75v.75m0 0H3.75m0 0h-.375a1.125 1.125 0 01-1.125-1.125V15m1.5 1.5v-.75A.75.75 0 003 15h-.75M15 10.5a3 3 0 11-6 0 3 3 0 016 0zm3 0h.008v.008H18V10.5zm-12 0h.008v.008H6V10.5z"></path>
+         </svg>
+          <h2 className="mt-4 text-xl font-bold text-white">
+          Pay After Delivery
+          </h2>
+          <p className="mt-1 text-sm text-gray-300">
+          We genuinely trust our clients, which is why we have created the Pay After Delivery model. With Pay After Delivery, you can pay within five days of the translation&apos;s delivery via credit card, bank transfer or Paypal.
+          </p>
+        </a>
+        <a
+          className="block rounded-xl border border-gray-800 p-8 shadow-xl transition hover:border-teal-500/10 hover:shadow-teal-500/10"
+          href="/services/digital-campaigns"
+        >
+        <svg 
+        className="h-10 w-10 text-[#B3FF17]"
+        fill="none" 
+        stroke="currentColor" 
+        stroke-width="1.5" 
+        viewBox="0 0 24 24" 
+        xmlns="http://www.w3.org/2000/svg" 
+        aria-hidden="true">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+        </svg>
+          <h2 className="mt-4 text-xl font-bold text-white">
+           On-Time Delivery
+          </h2>
+          <p className="mt-1 text-sm text-gray-300">
+          We offer the best performance levels in the industry, with an optimized workflow that guarantees over 95% of deliveries on time. 
+          Plus, in the unlikely event we miss a deadline, we will refund the translation up to its full cost.
+          </p>
+        </a>
+        
+      </div>
+      <div className="mt-12 text-center">
+        <a
+          href="#"
+          className="inline-block rounded bg-[#B3FF17] px-12 py-3 text-sm font-medium text-black transition hover:bg-teal-100 focus:outline-none focus:ring focus:ring-yellow-400"
+        >
+          Order Today
+        </a>
+      </div>
+  </div>
+  <div className='w-full max-w-6xl mx-auto overflow-hidden sm:rounded-2xl'>
+    <div className="bg-white py-24 sm:py-32 mt-4">
+    <div className="mx-auto max-w-7xl px-6 lg:px-8">
+      <div className="mx-auto max-w-2xl lg:text-center">
+        <h2 className="text-base font-semibold leading-7 text-indigo-600">
+          Deploy faster
+        </h2>
+        <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+          Everything you need to deploy your app
+        </p>
+        <p className="mt-6 text-lg leading-8 text-gray-600">
+          Quis tellus eget adipiscing convallis sit sit eget aliquet quis.
+          Suspendisse eget egestas a elementum pulvinar et feugiat blandit at.
+          In mi viverra elit nunc.
+        </p>
+      </div>
+      <div className="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl">
+        <dl className="grid max-w-xl grid-cols-1 gap-y-10 gap-x-8 lg:max-w-none lg:grid-cols-2 lg:gap-y-16">
+          <div className="relative pl-16">
+            <dt className="text-base font-semibold leading-7 text-gray-900">
+              <div className="absolute top-0 left-0 flex h-10 w-10 items-center justify-center rounded-lg bg-indigo-600">
+                <svg
+                  className="h-6 w-6 text-white"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M12 16.5V9.75m0 0l3 3m-3-3l-3 3M6.75 19.5a4.5 4.5 0 01-1.41-8.775 5.25 5.25 0 0110.233-2.33 3 3 0 013.758 3.848A3.752 3.752 0 0118 19.5H6.75z"
+                  />
+                </svg>
+              </div>
+              Push to deploy
+            </dt>
+            <dd className="mt-2 text-base leading-7 text-gray-600">
+              Morbi viverra dui mi arcu sed. Tellus semper adipiscing
+              suspendisse semper morbi. Odio urna massa nunc massa.
+            </dd>
+          </div>
+          <div className="relative pl-16">
+            <dt className="text-base font-semibold leading-7 text-gray-900">
+              <div className="absolute top-0 left-0 flex h-10 w-10 items-center justify-center rounded-lg bg-indigo-600">
+                <svg
+                  className="h-6 w-6 text-white"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z"
+                  />
+                </svg>
+              </div>
+              SSL certificates
+            </dt>
+            <dd className="mt-2 text-base leading-7 text-gray-600">
+              Sit quis amet rutrum tellus ullamcorper ultricies libero dolor
+              eget. Sem sodales gravida quam turpis enim lacus amet.
+            </dd>
+          </div>
+          <div className="relative pl-16">
+            <dt className="text-base font-semibold leading-7 text-gray-900">
+              <div className="absolute top-0 left-0 flex h-10 w-10 items-center justify-center rounded-lg bg-indigo-600">
+                <svg
+                  className="h-6 w-6 text-white"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99"
+                  />
+                </svg>
+              </div>
+              Simple queues
+            </dt>
+            <dd className="mt-2 text-base leading-7 text-gray-600">
+              Quisque est vel vulputate cursus. Risus proin diam nunc commodo.
+              Lobortis auctor congue commodo diam neque.
+            </dd>
+          </div>
+          <div className="relative pl-16">
+            <dt className="text-base font-semibold leading-7 text-gray-900">
+              <div className="absolute top-0 left-0 flex h-10 w-10 items-center justify-center rounded-lg bg-indigo-600">
+                <svg
+                  className="h-6 w-6 text-white"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M7.864 4.243A7.5 7.5 0 0119.5 10.5c0 2.92-.556 5.709-1.568 8.268M5.742 6.364A7.465 7.465 0 004.5 10.5a7.464 7.464 0 01-1.15 3.993m1.989 3.559A11.209 11.209 0 008.25 10.5a3.75 3.75 0 117.5 0c0 .527-.021 1.049-.064 1.565M12 10.5a14.94 14.94 0 01-3.6 9.75m6.633-4.596a18.666 18.666 0 01-2.485 5.33"
+                  />
+                </svg>
+              </div>
+              Advanced security
+            </dt>
+            <dd className="mt-2 text-base leading-7 text-gray-600">
+              Arcu egestas dolor vel iaculis in ipsum mauris. Tincidunt mattis
+              aliquet hac quis. Id hac maecenas ac donec pharetra eget.
+            </dd>
+          </div>
+        </dl>
+      </div>
+    </div>
+  </div>
+
+ 
+    
+  </div>
   </section>
 
 )};

57f28ee541cf008629eff0a6939316fbe5319c0d
Henoker <hennybany@gmail.com>
2023-04-13 14:49:14 +0300

    fix: removed unnecessary dependencies

--- sections/MarqueeSlider.jsx
+++ sections/MarqueeSlider.jsx
@@ -91,36 +91,7 @@ const MarqueeSlider = () => {
     </div>
   </section>
   </section>
-  //   <div className="bg-white h-[160px] overflow-hidden">
-  //   <Marquee>
-  //     <div className="flex items-center space-x-2">
-  //     <span className="text-lg font-bold text-gray-800">Our Clients</span>
-  //       <img src="/fifa.svg" alt="logo" className="w-[100px] h-[100px]"/>
-  //     </div>
-      
-  //     <div className="flex items-center space-x-2 mt-2">
-  //       <img src="https://upload.wikimedia.org/wikipedia/commons/7/71/International_Rescue_Committee_Logo.svg" alt="logo" className="w-[150px] h-[150px]" />
-  //     </div>
-  //     <div className="flex items-center space-x-2 mt-2">
-  //       <img src="https://upload.wikimedia.org/wikipedia/commons/7/75/Ethiopian_Airlines_Logo.svg" alt="logo" className="w-[150px] h-[150px]" />
-  //     </div>
-  //     <div className="flex items-center space-x-2 mt-2">
-  //       <img src="https://upload.wikimedia.org/wikipedia/commons/d/d0/Emirates_logo.svg" alt="logo" className="w-[150px] h-[150px]" />
-  //     </div>
-  //     <div className="flex items-center space-x-2 mt-2">
-  //       <img src="https://upload.wikimedia.org/wikipedia/commons/9/9f/UNDP_logo.svg" alt="logo" className="w-[150px] h-[150px]" />
-  //     </div>
-  //     <div className="flex items-center space-x-2 mt-2">
-  //       <img src="https://upload.wikimedia.org/wikipedia/commons/b/b8/Flag_of_ILO.svg" alt="logo" className="w-[150px] h-[150px]" />
-  //     </div>
-  //     <div className="flex items-center space-x-2 mt-2">
-  //       <img src="https://upload.wikimedia.org/wikipedia/commons/b/b0/UNESCO_logo_English.svg" alt="logo" className="w-[150px] h-[150px]" />
-  //     </div>
-  //     <div className="flex items-center space-x-2 mt-2">
-  //       <img src="https://upload.wikimedia.org/wikipedia/commons/9/9c/CURE_International_Logo.png" alt="logo" className="w-[150px] h-[150px]" />
-  //     </div>
-  //   </Marquee>
-  // </div>
+ 
 
   );
 };

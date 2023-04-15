5d365e10d7e1a6036940015f3f25c7815af18da5
Henoker <hennybany@gmail.com>
2023-04-15 12:32:00 +0300

    feat: feeback page server issues

--- sections/Feedback.jsx
+++ sections/Feedback.jsx
@@ -1,15 +1,17 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable max-len */
+/* eslint-disable jsx-a11y/anchor-is-valid */
+/* eslint-disable react/jsx-props-no-multi-spaces */
+
 'use client';
 
-import { useState } from "react";
-
-
+import { useState } from 'react';
 
 const Feedback = () => {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [message, setMessage] = useState("");
-  const [alertMessage, setAlertMessage] = useState("");
-
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+  const [alertMessage, setAlertMessage] = useState('');
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -20,37 +22,43 @@ const Feedback = () => {
       message,
     };
 
-    const rawResponse = await fetch("/api/submit", {
-      method: "POST",
+    const rawResponse = await fetch('/api/submit', {
+      method: 'POST',
       headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify(form),
     });
-    const content = await rawResponse.json();
 
-    setAlertMessage("Your message sent successfully");
- 
+    try {
+      const content = await rawResponse.json();
+      setAlertMessage('Your message sent successfully');
+    // eslint-disable-next-line no-shadow
+    } catch (e) {
+      console.error(e);
+      setAlertMessage('There was an error submitting your message.');
+    }
+
     // Reset the form fields
-    setMessage("");
-    setName("");
-    setEmail("");
+    setMessage('');
+    setName('');
+    setEmail('');
   };
 
   return (
     <div className="container px-6 py-12 mx-auto">
-    
-       <div className="lg:flex lg:items-center lg:-mx-6">
-       
-       <div className="lg:w-1/2 lg:mx-6">
-     
-         <h1 className="text-2xl font-semibold capitalize text-white lg:text-3xl">
+
+      <div className="lg:flex lg:items-center lg:-mx-6">
+
+        <div className="lg:w-1/2 lg:mx-6">
+
+          <h1 className="text-2xl font-semibold capitalize text-white lg:text-3xl">
             Contact us for <br /> more info
           </h1>
           <div className="mt-6 space-y-8 md:mt-8">
-           <p className="flex items-start -mx-2">
-             <svg
+            <p className="flex items-start -mx-2">
+              <svg
                 xmlns="http://www.w3.org/2000/svg"
                 className="w-6 h-6 mx-2 text-blue-400 "
                 fill="none"
@@ -188,33 +196,34 @@ const Feedback = () => {
           </div>
         </div>
         <div className="mt-8 lg:w-1/2 lg:mx-6">
-       
+
           <div className="w-full px-8 py-10 mx-auto overflow-hidden rounded-lg shadow-2xl bg-[#B3FF17] lg:max-w-xl shadow-black/50">
-          {alertMessage && (
+            {alertMessage && (
 
-            <div class="flex bg-green-lighter max-w-sm mb-4">
-            <div class="w-16 bg-green">
-                <div class="p-4">
-                    <svg class="h-8 w-8 text-white fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M468.907 214.604c-11.423 0-20.682 9.26-20.682 20.682v20.831c-.031 54.338-21.221 105.412-59.666 143.812-38.417 38.372-89.467 59.5-143.761 59.5h-.12C132.506 459.365 41.3 368.056 41.364 255.883c.031-54.337 21.221-105.411 59.667-143.813 38.417-38.372 89.468-59.5 143.761-59.5h.12c28.672.016 56.49 5.942 82.68 17.611 10.436 4.65 22.659-.041 27.309-10.474 4.648-10.433-.04-22.659-10.474-27.309-31.516-14.043-64.989-21.173-99.492-21.192h-.144c-65.329 0-126.767 25.428-172.993 71.6C25.536 129.014.038 190.473 0 255.861c-.037 65.386 25.389 126.874 71.599 173.136 46.21 46.262 107.668 71.76 173.055 71.798h.144c65.329 0 126.767-25.427 172.993-71.6 46.262-46.209 71.76-107.668 71.798-173.066v-20.842c0-11.423-9.259-20.683-20.682-20.683z"/><path d="M505.942 39.803c-8.077-8.076-21.172-8.076-29.249 0L244.794 271.701l-52.609-52.609c-8.076-8.077-21.172-8.077-29.248 0-8.077 8.077-8.077 21.172 0 29.249l67.234 67.234a20.616 20.616 0 0 0 14.625 6.058 20.618 20.618 0 0 0 14.625-6.058L505.942 69.052c8.077-8.077 8.077-21.172 0-29.249z"/></svg>
+            <div className="flex bg-green-lighter max-w-sm mb-4">
+              <div className="w-16 bg-green">
+                <div className="p-4">
+                  <svg className="h-8 w-8 text-white fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M468.907 214.604c-11.423 0-20.682 9.26-20.682 20.682v20.831c-.031 54.338-21.221 105.412-59.666 143.812-38.417 38.372-89.467 59.5-143.761 59.5h-.12C132.506 459.365 41.3 368.056 41.364 255.883c.031-54.337 21.221-105.411 59.667-143.813 38.417-38.372 89.468-59.5 143.761-59.5h.12c28.672.016 56.49 5.942 82.68 17.611 10.436 4.65 22.659-.041 27.309-10.474 4.648-10.433-.04-22.659-10.474-27.309-31.516-14.043-64.989-21.173-99.492-21.192h-.144c-65.329 0-126.767 25.428-172.993 71.6C25.536 129.014.038 190.473 0 255.861c-.037 65.386 25.389 126.874 71.599 173.136 46.21 46.262 107.668 71.76 173.055 71.798h.144c65.329 0 126.767-25.427 172.993-71.6 46.262-46.209 71.76-107.668 71.798-173.066v-20.842c0-11.423-9.259-20.683-20.682-20.683z" /><path d="M505.942 39.803c-8.077-8.076-21.172-8.076-29.249 0L244.794 271.701l-52.609-52.609c-8.076-8.077-21.172-8.077-29.248 0-8.077 8.077-8.077 21.172 0 29.249l67.234 67.234a20.616 20.616 0 0 0 14.625 6.058 20.618 20.618 0 0 0 14.625-6.058L505.942 69.052c8.077-8.077 8.077-21.172 0-29.249z" /></svg>
                 </div>
-            </div>
-            <div class="w-auto text-grey-darker items-center p-4">
-                <span class="text-lg font-bold pb-4">
-                   Thank you!
+              </div>
+              <div className="w-auto text-grey-darker items-center p-4">
+                <span className="text-lg font-bold pb-4">
+                  Thank you!
                 </span>
-                <p class="leading-tight">
-                    {alertMessage}
+                <p className="leading-tight">
+                  {alertMessage}
                 </p>
+              </div>
             </div>
-        </div>
 
-           )}
+            )}
             <h1 className="text-lg font-medium text-gray-900">
               Contact Form
             </h1>
-            <form 
-            onSubmit={handleSubmit}
-            className="mt-6">
+            <form
+              onSubmit={handleSubmit}
+              className="mt-6"
+            >
               <div className="flex-1">
                 <label className="block mb-2 text-sm  text-gray-900">
                   Full Name
@@ -234,13 +243,12 @@ const Feedback = () => {
                   Email address
                 </label>
                 <input
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                type="email"
-                name="email"
-                id="email"
-                
-                 
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  type="email"
+                  name="email"
+                  id="email"
+
                   placeholder="johndoe@example.com"
                   required
                   className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
@@ -257,10 +265,10 @@ const Feedback = () => {
                   onChange={(e) => setMessage(e.target.value)}
                   id="message"
                   required
-                  defaultValue={""}
+                  defaultValue=""
                 />
               </div>
-              <button className="w-full px-6 py-3 mt-6 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-skin-button-accent hover:bg-skin-button-accent-hover rounded-md focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-50">
+              <button type="button" className="w-full px-6 py-3 mt-6 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-skin-button-accent hover:bg-skin-button-accent-hover rounded-md focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-50">
                 Send Message
               </button>
             </form>
@@ -268,9 +276,8 @@ const Feedback = () => {
         </div>
       </div>
     </div>
-   
+
   );
 };
-  
 
 export default Feedback;

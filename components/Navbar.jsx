354696956b4baf4fb1ef55344f52179e5869b51b
Henoker <hennybany@gmail.com>
2023-04-30 05:39:06 +0300

    fix: navabr auto close issues

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -44,45 +44,55 @@ const Navbar = () => {
         className={`w-full text-amber-400 opacity-90 mx-4 block flex-grow lg:flex lg:items-center lg:w-auto ${isOpen ? 'block' : 'hidden'}`}
       >
         <div className="text-sm lg:flex-grow bg-white md:bg-transparent">
-          <Link href="/" className="block mt-4 lg:inline-block lg:mt-0 text-blue-700 md:text-blue-200 hover:text-blue-500 hover:underline mx-4">
+          <Link
+            href="/"
+            className="block mt-4 lg:inline-block lg:mt-0 text-blue-700 md:text-blue-200 hover:text-blue-500 hover:underline mx-4"
+            onClick={() => setIsOpen(!isOpen)}
+          >
             Home
           </Link>
-          <Link href="/about" className="block mt-4 lg:inline-block lg:mt-0 text-blue-700 md:text-blue-200 hover:text-blue-500 hover:underline  mx-4">
+          <Link
+            href="/about"
+            className="block mt-4 lg:inline-block lg:mt-0 text-blue-700 md:text-blue-200 hover:text-blue-500 hover:underline  mx-4"
+            onClick={() => setIsOpen(!isOpen)}
+          >
             About
           </Link>
           <Link
             href="/services"
             className="block mt-4 lg:inline-block lg:mt-0 text-blue-700 md:text-blue-200 hover:text-blue-500 hover:underline ml-4"
-            onClick={() => setShowSubMenu(!showSubMenu)}
+            onClick={() => setIsOpen(!isOpen)}
           >
             Services
-            <div className="relative inline-block">
-              <button
-                type="button"
-                className="relative z-10 block p-2 text-gray-700 border border-transparent rounded-md focus:border-blue-500 focus:ring-opacity-40 dark:focus:ring-opacity-40 focus:ring-blue-300 dark:focus:ring-blue-400 focus:outline-none"
-                onClick={handleServicesClick}
+          </Link>
+          <div className="relative inline-block">
+            <button
+              type="button"
+              className="relative z-10 block p-2 text-gray-700 border border-transparent rounded-md focus:border-blue-500 focus:ring-opacity-40 dark:focus:ring-opacity-40 focus:ring-blue-300 dark:focus:ring-blue-400 focus:outline-none"
+              onClick={handleServicesClick}
+            >
+              <svg
+                className="w-5 h-5 text-blue-800 lg:text-white"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
               >
-                <svg
-                  className="w-5 h-5 text-blue-800 lg:text-white"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                    clipRule="evenodd"
-                  />
-                </svg>
-              </button>
-              {/* Dropdown menu */}
-              {showSubMenu && (
+                <path
+                  fillRule="evenodd"
+                  d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </button>
+            {/* Dropdown menu */}
+            {showSubMenu && (
               <div
                 className="absolute left-0 z-20 w-48 py-2 ml-0 mt-2 origin-top-right bg-white rounded-md shadow-xl dark:bg-gray-800"
               >
                 <Link
                   href="/services/translation"
                   className="block px-4 py-3 text-sm text-gray-600 capitalize transition-colors duration-300 transform dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 dark:hover:text-white"
+                  onClick={() => setShowSubMenu(!showSubMenu)}
                 >
                   {' '}
                   Translation{' '}
@@ -90,6 +100,7 @@ const Navbar = () => {
                 <Link
                   href="/services/localization"
                   className="block px-4 py-3 text-sm text-gray-600 capitalize transition-colors duration-300 transform dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 dark:hover:text-white"
+                  onClick={() => setShowSubMenu(!showSubMenu)}
                 >
                   {' '}
                   Localization{' '}
@@ -97,6 +108,7 @@ const Navbar = () => {
                 <Link
                   href="/services/interpretation"
                   className="block px-4 py-3 text-sm text-gray-600 capitalize transition-colors duration-300 transform dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 dark:hover:text-white"
+                  onClick={() => setShowSubMenu(!showSubMenu)}
                 >
                   {' '}
                   Interpretation{' '}
@@ -104,6 +116,7 @@ const Navbar = () => {
                 <Link
                   href="/services/audio-services"
                   className="block px-4 py-3 text-sm text-gray-600 capitalize transition-colors duration-300 transform dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 dark:hover:text-white"
+                  onClick={() => setShowSubMenu(!showSubMenu)}
                 >
                   {' '}
                   Audio Services{' '}
@@ -111,6 +124,7 @@ const Navbar = () => {
                 <Link
                   href="/services/dtp"
                   className="block px-4 py-3 text-sm text-gray-600 capitalize transition-colors duration-300 transform dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 dark:hover:text-white"
+                  onClick={() => setShowSubMenu(!showSubMenu)}
                 >
                   {' '}
                   DTP{' '}
@@ -118,18 +132,26 @@ const Navbar = () => {
                 <Link
                   href="/services/ai-and-machine-translation"
                   className="block px-4 py-3 text-sm text-gray-600 capitalize transition-colors duration-300 transform dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 dark:hover:text-white"
+                  onClick={() => setShowSubMenu(!showSubMenu)}
                 >
                   {' '}
                   Machine Translation{' '}
                 </Link>
               </div>
-              )}
-            </div>
-          </Link>
-          <Link href="/industries" className="block mt-4 lg:inline-block lg:mt-0 text-blue-700 md:text-blue-200 hover:text-blue-500 hover:underline  mx-4">
+            )}
+          </div>
+          <Link
+            href="/industries"
+            className="block mt-4 lg:inline-block lg:mt-0 text-blue-700 md:text-blue-200 hover:text-blue-500 hover:underline  mx-4"
+            onClick={() => setIsOpen(!isOpen)}
+          >
             Industries
           </Link>
-          <Link href="/quote" className="block bg-amber-500 border-0 py-2 px-4 mt-4 lg:inline-block lg:mt-0 text-blue-700 md:text-gray-200 hover:text-blue-500 hover:underline  mx-4">
+          <Link
+            href="/quote"
+            className="block bg-amber-500 border-0 py-2 px-4 mt-4 lg:inline-block lg:mt-0 text-blue-700 md:text-gray-200 hover:text-blue-500 hover:underline  mx-4"
+            onClick={() => setIsOpen(!isOpen)}
+          >
             Instant Quote
           </Link>
         </div>

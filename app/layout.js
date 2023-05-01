05d564c13fe251e93f4be2e2d1f2e531bcb4786d
Henoker <hennybany@gmail.com>
2023-05-01 22:23:19 +0300

    feat: metadata issues

--- app/layout.js
+++ app/layout.js
@@ -2,46 +2,12 @@ import { Navbar, Footer } from '../components';
 import '../styles/globals.css';
 
 export const metadata = {
-  title: {
-
-    template: '%s | Ethiostar America LLC',
-  },
-  description: 'Ethiostar America is a Fremont, California-based translation and localization LLC company. We provide document translation, proofreading, mobile localization, website localization and software localization, interpretation, voiceover, desktop publishing, and more.',
-  keywords: [
-    'Ethiostar America', 'african languages', 'translation', 'localization', 'document translation', 'proofreading', 'mobile localization', 'website localization', 'software localization', 'interpretation', 'voiceover', 'desktop publishing', 'Fremont, California',
-  ],
-  authors: [
-    {
-      name: 'Ethiostar America',
-      url: 'https://ethiostaramerica.com',
-    },
-  ],
-  creator: 'Ethiostar America',
-  openGraph: {
-    type: 'website',
-    locale: 'en_US',
-    url: 'https://www.ethiostaramerica.com/',
-    title: 'Ethiostar America - Translation and Localization LLC',
-    description: 'Ethiostar America is a Fremont, California-based translation and localization LLC company. We provide document translation, proofreading, mobile localization, website localization and software localization, interpretation, voiceover, desktop publishing, and more.',
-    siteName: 'Ethiostar America',
-  },
-  // twitter: {
-  //   card: 'summary_large_image',
-  //   title: 'Ethiostar America - Translation and Localization LLC',
-  //   description: 'Ethiostar America is a Fremont, California-based translation and localization LLC company. We provide document translation, proofreading, mobile localization, website localization and software localization, interpretation, voiceover, desktop publishing, and more.',
-  //   images: [`${siteConfig.url}/og.jpg`],
-  //   creator: '@shadcn',
-  // },
-  icons: {
-    icon: '/favicon.ico',
-    shortcut: '/favicon-16x16.png',
-    apple: '/apple-touch-icon.png',
-  },
+  title: 'My Title',
 };
 
 const RootLayout = ({ children }) => (
   <html lang="en">
-    {/* <head>
+    <head>
       <link rel="preconnect" href="https://stijndv.com" />
       <link rel="stylesheet" href="https://stijndv.com/fonts/Eudoxus-Sans.css" />
       <title>Ethiostar America LLC</title>
@@ -61,7 +27,7 @@ const RootLayout = ({ children }) => (
       <meta name="twitter:image" content="/ethiostar-america-logo.png" />
       <meta name="twitter:card" content="summary_large_image" />
       <link rel="icon" href="/favicon.ico" />
-    </head> */}
+    </head>
     <body>
       <header>
         <Navbar />
--- app/page.js
+++ app/page.js
@@ -1,28 +1,7 @@
-// import { NextSeo } from 'next-seo';
 import { About, Intro, Feedback, MarqueeSlider, GetStarted, Hero, Insights, WhatsNew, World } from '../sections';
 
 const Page = () => (
   <div className="theme-dark-blue bg-skin-fill">
-    {/* <NextSeo
-      title="Home: Ethiostar America LLC"
-      description="Ethiostar America is a Fremont, California-based translation and localization LLC company. We provide document translation, proofreading, mobile localization, website localization and software localization, interpretation, voiceover, desktop publishing, and more."
-      creator="Ethiostar America"
-      openGraph={{
-        type: 'website',
-        url: 'https://ethiostaramerica.com/home',
-        title: 'Ethiostar America',
-        description: 'Ethiostar America is a Fremont, California-based translation and localization LLC company. We provide document translation, proofreading, mobile localization, website localization and software localization, interpretation, voiceover, desktop publishing, and more.',
-        images: [
-          {
-            url: 'https://www.mywebsite.com/my-page-image.jpg',
-            width: 1200,
-            height: 630,
-            alt: 'My Page Image',
-          },
-        ],
-      }}
-    />; */}
-
     <Hero />
     <div>
       <About />

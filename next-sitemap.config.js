f4c51d2319c1ac936cf344379a3e2b30cdd6863e
Henoker <hennybany@gmail.com>
2023-05-07 12:37:31 +0300

    fear: fix SEO index issues

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,4 +1,16 @@
 module.exports = {
   siteUrl: 'https://www.ethiostaramerica.com/',
   generateRobotsTxt: true,
+  robotsTxtOptions: {
+    content: `
+      User-agent: *
+      Disallow:
+
+      # Host
+      Host: https://www.ethiostaramerica.com/
+
+      # Sitemaps
+      Sitemap: https://www.ethiostaramerica.com/sitemap.xml
+    `,
+  },
 };

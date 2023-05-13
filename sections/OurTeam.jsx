ead2e1c69cd63331041cbc86296788298be2e2af
Henoker <hennybany@gmail.com>
2023-05-13 23:18:53 +0300

    feat: updated team photo

--- sections/OurTeam.jsx
+++ sections/OurTeam.jsx
@@ -71,10 +71,10 @@ const OurTeam = () => (
           </div>
         </div>
         <div className="flex flex-col justify-center bg-white w-full px-8 mx-6 my-12 text-center rounded-md md:w-96 lg:w-80 xl:w-64 dark:bg-gray-100 dark:text-gray-800">
-          <img alt="" className="self-center flex-shrink-0 w-24 h-24 -mt-12 bg-center bg-cover rounded-full dark:bg-gray-500" src="https://ethiostarlocalization.com/wp-content/uploads/2022/12/1667649966301.jpeg" />
+          <img alt="" className="self-center flex-shrink-0 w-24 h-24 -mt-12 bg-center bg-cover rounded-full dark:bg-gray-500" src="/Ephesonmug.jpg" />
           <div className="flex-1 my-4">
-            <p className="text-xl font-semibold leading-snug">Zainab Mohammed</p>
-            <p>Project Manager</p>
+            <p className="text-xl font-semibold leading-snug">Epheson B Tesfatsion</p>
+            <p>Sales Manager</p>
           </div>
           <div className="flex items-center justify-center p-3 space-x-3 border-t-2">
             <a rel="noopener noreferrer" href="#" title="Email" className="dark:text-gray-900 hover:dark:text-violet-400">

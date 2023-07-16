4262b75bdee63d13d97dd7b33670ee761defa697
Henoker <hennybany@gmail.com>
2023-07-16 21:50:25 +0300

    feat: changes testmonial content on services page

--- components/TestimonialSolo.jsx
+++ components/TestimonialSolo.jsx
@@ -5,19 +5,19 @@ const TestimonialSolo = () => (
       <div className="grid grid-cols-1 gap-12 sm:grid-cols-3 sm:items-center">
         <img
           alt="Man"
-          src="https://images.unsplash.com/photo-1520341280432-4749d4d7bcf9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
-          className="aspect-square w-full rounded-lg object-cover"
+          src="https://upload.wikimedia.org/wikipedia/en/e/ef/International_Organization_for_Migration_logo.png"
+          className="aspect-square w-full rounded-lg object-fit"
         />
         <blockquote className="sm:col-span-2">
           <p className="text-xl font-medium sm:text-2xl">
-            I needed to translate a complex legal document into several languages, and I was blown away by the quality
-            and professionalism of the translation services provided by this company.
-            They delivered the translations on time.
+            I would like to write a few words to also express my appreciation to you and your colleagues at Ethiostar Translation PLC.
+            We are happy and satisfied with the various translations of our Canadian Orientation Abroad (COA) programme materials.
+            You have, no doubt, learned a great deal about Canada in that process!
           </p>
           <cite className="mt-8 inline-flex items-center not-italic">
             <span className="hidden h-px w-6 bg-gray-400 sm:inline-block" />
             <p className="text-sm uppercase text-gray-500 sm:ml-3">
-              <strong>Justin Harrison</strong>, Beatle Inc.
+              <strong>International Organization for Migration</strong>,swizerland.
             </p>
           </cite>
         </blockquote>
